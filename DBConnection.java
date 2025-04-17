2f18bb9c583355b3e4543137d129525d9b7b8305
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-04-17 10:19:10 +0530

    Update DBConnection.java by defining private static final variables for MySQL username and password

--- DBConnection.java
+++ DBConnection.java
@@ -6,4 +6,8 @@ import java.sql.*;
 public class DBConnection {
   // Database connection URL (make sure 'studentdb' exists in your MySQL server)
   private static final String URL = "jdbc:mysql://localhost:3306/studentdb";
+
+  // MySQL username and password
+  private static final String USER = "enter here"; // The username of your MySQL Workbench
+  private static final String PASSWORD = "enter here"; // The password of your MYSQl Workbench
   
