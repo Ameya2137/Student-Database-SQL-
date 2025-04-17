6b80bb0c5aa8ab6770839b0888ca289767d90b88
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-04-17 10:25:02 +0530

    Create StudentOperations.java

---
+++ StudentOperations.java
@@ -0,0 +1,14 @@
+//StudentOperations.java
+
+import java.util.*;
+
+// Handles operations related to students at the application level
+public class StudentOperations {
+
+    // Instance of StudentDAO to interact with the database
+    private StudentDAO dao;
+
+    // Constructor initializes the DAO object
+    public StudentOperations() {
+        dao = new StudentDAO();
+    }
