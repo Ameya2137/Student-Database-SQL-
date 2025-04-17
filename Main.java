f91582857735cbe9515c2f4909f797223be1204b
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-04-17 09:43:04 +0530

    Create Main.java and adding a void main function with a Scanner object

---
+++ Main.java
@@ -0,0 +1,14 @@
+//Main.java
+
+//Name: Ameya Tipnis
+//PRN: 23070126012
+//Dept: SY-AIML-A1
+
+// Importing required classes
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);  // Scanner for user input
+        StudentOperations operations = new StudentOperations();  // Object to perform operations on students
+        int choice;
