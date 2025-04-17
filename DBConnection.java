f48a7c3d7ef9458e54b66b85107ac5bd636f1770
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-04-17 10:16:40 +0530

    Update DBConnection.java by writing the Database connection URL

--- DBConnection.java
+++ DBConnection.java
@@ -4,3 +4,6 @@ import java.sql.*;
 
 // Utility class to manage database connection
 public class DBConnection {
+  // Database connection URL (make sure 'studentdb' exists in your MySQL server)
+  private static final String URL = "jdbc:mysql://localhost:3306/studentdb";
+  
