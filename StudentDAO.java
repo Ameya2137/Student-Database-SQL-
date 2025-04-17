39e6e9a192b3ebfdae33e8f6c2be65efbf042c90
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-04-17 09:51:53 +0530

    Create StudentDAO.java

---
+++ StudentDAO.java
@@ -0,0 +1,9 @@
+//StudentDAO.java
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class StudentDAO {
+
+    // Adding a new student to the database
+    public void addStudent(Student student) {
